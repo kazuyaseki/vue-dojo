1e772c3b8ec11ddc1a811b48d0d21de6d10eac3a
kazuya-seki <track613armory@gmail.com>
2018-01-29 10:29:45 +0900

    added first single file component

---
+++ advanced-fine-grained-components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 @click="onClick">
+    {{message}}
+  </h1>
+</template>
+
+<script>
+import Vue from "vue"
+import Component from "vue-class-component"
+
+export default Component({})(
+class App extends Vue {
+  constructor(){
+    super();
+    this.message = "Hello from class"
+  }
+
+  onClick() {
+    this.message = "Goodbye"
+  }
+})
+</script>
---
+++ advanced-fine-grained-components/index.js
@@ -0,0 +1,8 @@
+//import "tailwindcss/dist/tailwind.css";
+import Vue from "vue";
+import App from "./App.vue";
+
+new Vue({
+  el: "#root",
+  render: h => <App />
+});

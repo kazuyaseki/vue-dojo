37a429daaebc08d2c71a98a2bfe7ac0b55ae304b
kazuya-seki <track613armory@gmail.com>
2018-01-30 16:41:52 +0900

    added Settings component to hold constant values

--- advanced-fine-grained-components/App.vue
+++ advanced-fine-grained-components/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <Layout>
-  </Layout>
+  <Settings>
+    <Layout slot-scope="{header,footer}">
+      <h1 slot="header">{{header}}</h1>
+      <div slot="content">Amazing content</div>
+      <h2 slot="footer">{{footer}}</h2>
+    </Layout>
+  </Settings>
 </template>
 
 <script>
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 import Layout from "./Layout.vue";
+import Settings from "./Settings.vue";
 
 @Component({
   components: {
-    Layout
+    Layout,
+    Settings
   }
 })
 export default class App extends Vue {
---
+++ advanced-fine-grained-components/Settings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot :header="header" :footer="footer"></slot>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+export default class Settings extends Vue {
+  header = "Awesome Site";
+  footer = "Incredible Footer";
+}
+</script>

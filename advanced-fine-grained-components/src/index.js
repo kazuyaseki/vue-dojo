582c401f512bfcb456208e01c012ee78dcea5076
kazuya-seki <track613armory@gmail.com>
2018-01-30 17:05:50 +0900

    forgot to set back app component child

--- advanced-fine-grained-components/src/index.js
+++ advanced-fine-grained-components/src/index.js
@@ -4,10 +4,5 @@ import App from "./App.vue";
 
 new Vue({
   el: "#root",
-  render: h => (
-    <App message="aaa">
-      <h2 slot="header">Header</h2>
-      <h3 slot="footer">Footer</h3>
-    </App>
-  )
+  render: h => <App />
 });

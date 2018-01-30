14499bd253800b6122dc8573fbaf77ed7981b5f2
kazuya-seki <track613armory@gmail.com>
2018-01-30 17:41:00 +0900

    modified to render cats with inline functional component

--- advanced-fine-grained-components/src/App.vue
+++ advanced-fine-grained-components/src/App.vue
@@ -2,13 +2,7 @@
   <Settings>
     <Layout slot-scope="{header,footer}">
       <AwesomeHeader slot="header" :header="header" />
-      <AwesomeContent slot="content" :limit="4">
-        <div><img src="https://robohash.org/mindy?set=set4" alt=""></div>
-        <div><img src="https://robohash.org/john?set=set4" alt=""></div>
-        <div><img src="https://robohash.org/kim?set=set4" alt=""></div>
-        <div><img src="https://robohash.org/joel?set=set4" alt=""></div>
-        <div><img src="https://robohash.org/maggie?set=set4" alt=""></div>
-      </AwesomeContent>
+      <Cats slot="content" :names="names" :limit="limit"></Cats>
       <AwesomeFooter slot="footer" :footer="footer" />
     </Layout>
   </Settings>
@@ -21,21 +15,30 @@ import Layout from "./Layout.vue";
 import Settings from "./Settings.vue";
 import { Content, Header, Footer } from "./components";
 
+const Cats = {
+  functional: true,
+  render: (h, { props }) => (
+    <div>
+      {props.names
+        .map(name => `https://robohash.org/${name}?set=set4`)
+        .map(url => <img src={url} alt="" />)
+        .slice(0, props.limit)}
+    </div>
+  )
+};
+
 @Component({
   components: {
     AwesomeContent: Content,
     AwesomeHeader: Header,
     AwesomeFooter: Footer,
+    Cats,
     Layout,
     Settings
   }
 })
 export default class App extends Vue {
-  @Prop({ default: "this is default msg" })
-  message;
-
-  onClick() {
-    this.message = "Goodbye";
-  }
+  names = ["mindy", "john", "kim", "joel", "ben"];
+  limit = 3;
 }
 </script>

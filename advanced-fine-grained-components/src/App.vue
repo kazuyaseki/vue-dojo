c7d2903219b324c1be08c3d73e4850dfa1d0b7e9
kazuya-seki <track613armory@gmail.com>
2018-01-31 13:09:32 +0900

    learned how to switch components using v-model binding

--- advanced-fine-grained-components/src/App.vue
+++ advanced-fine-grained-components/src/App.vue
@@ -2,6 +2,12 @@
   <Settings>
     <Layout slot-scope="{header,footer}">
       <AwesomeHeader slot="header" :header="header" />
+      <div slot="content" class="flex-grow">
+        <select v-model="selectedComp">
+          <option v-for="comp in comps" :key="comp.name" :value="comp">{{comp.name}}</option>
+        </select>
+        <component :is="selectedComp"></component>
+      </div>
       <Cats slot="content" :names="names" :limit="limit"></Cats>
       <AwesomeFooter slot="footer" :footer="footer" />
     </Layout>
@@ -14,6 +20,24 @@ import { Component, Prop } from "vue-property-decorator";
 import Layout from "./Layout.vue";
 import Settings from "./Settings.vue";
 import { Cats, Content, Header, Footer } from "./components";
+
+const One = {
+  functional: true,
+  name: "One",
+  render: h => <h1 class="bg-red">One</h1>
+};
+
+const Two = {
+  functional: true,
+  name: "Two",
+  render: h => <h1 class="bg-green">Two</h1>
+};
+
+const Three = {
+  functional: true,
+  name: "Three",
+  render: h => <h1 class="bg-purple">Three</h1>
+};
 
 @Component({
   components: {
@@ -28,5 +52,8 @@ import { Cats, Content, Header, Footer } from "./components";
 export default class App extends Vue {
   names = ["mindy", "john", "kim", "joel", "ben"];
   limit = 3;
+
+  comps = [One, Two, Three];
+  selectedComp = this.comps[0];
 }
 </script>

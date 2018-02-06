2e26afa3baa130e0cb94d4824bfe91c8b95ec60b
kazuya-seki <track613armory@gmail.com>
2018-02-06 16:31:30 +0900

    learned how to handle error

---
+++ ssr/pages/error.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="dark-red">
+    Erorrrrrrrrr!!!!
+  </h1>
+</template>
--- ssr/pages/index.vue
+++ ssr/pages/index.vue
@@ -15,9 +15,17 @@ import { mapState, mapMutations } from "vuex";
 import axios from "axios";
 
 export default {
-  async fetch({ store }) {
-    const res = await axios.get("https://todos-cuvsmolowg.now.sh/todos");
-    store.commit("init", res.data);
+  async fetch({ store, redirect, error }) {
+    try {
+      const res = await axios.get("https://todos-cuvsmolowg.now.sh/todos");
+      store.commit("init", res.data);
+    } catch (err) {
+      //there are mainly 3 ways to handle error
+
+      //error({ statusCode: 500, message: "Oops!Sorry sir." });
+      redirect("/error");
+      //store.commit("init", []);
+    }
   },
   computed: {
     ...mapState({

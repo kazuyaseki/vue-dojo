58c60668f3f3879af4557dbe37934c2cc4464950
kazuya-seki <track613armory@gmail.com>
2018-02-06 11:09:56 +0900

    learned how to use watch

--- typescript/src/components/Hello.vue
+++ typescript/src/components/Hello.vue
@@ -3,12 +3,13 @@
     <h1 v-colorDirective="{ color: 'white', background: '#25aeac'}">{{ fullMessage }}</h1>
     <router-link to="hello-ts">HelloTS</router-link>
     <MyCheckbox :title="checkbox.title" :value="checkbox.value" v-model="checkbox.checked"/>
+    <button @click="sumUp">Hit me</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { Component, Prop } from "vue-property-decorator";
+import { Component, Prop, Watch } from "vue-property-decorator";
 import colorDirective from "../color-directive";
 import MyCheckbox from "./MyCheckbox.vue";
 
@@ -27,6 +28,15 @@ export default class Hello extends Vue {
     value: "checkbox-id",
     checked: true
   };
+  count = {
+    acum: 0
+  };
+
+  //deep:true を指定することによってオブジェクト内の値の変更も検知できるようになる
+  @Watch("count", { deep: true })
+  watchCount(newVal, oldVal) {
+    console.log(newVal, oldVal);
+  }
 
   @Prop({ type: String, default: "World" })
   msg: string;
@@ -36,8 +46,8 @@ export default class Hello extends Vue {
     return `${this.message} ${this.msg}`;
   }
 
-  created() {
-    console.log("created");
+  sumUp() {
+    this.count.acum++;
   }
 }
 </script>

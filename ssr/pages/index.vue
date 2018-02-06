c3143d0ae41cf58c32f2a144e251697d6895441c
kazuya-seki <track613armory@gmail.com>
2018-02-06 16:24:13 +0900

    added fetch hook to execute async operation and set initial data loaded with http request

--- ssr/pages/index.vue
+++ ssr/pages/index.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="pv4 dark-red">
-    {{ counter }}
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
+    <article class="pa3 pa5-ns">
+      <h1 class="f4 bold center mw6">Todos</h1>
+      <ul class="list pl0 ml0 center mw6 ba b--light-silver br2">
+        <li :key="todo.task" v-for="todo of todos" class="ph3 pv3 bb b--light-silver">
+          {{todo.task}}
+        </li>
+      </ul>
+    </article>
   </div>
 </template>
 <script>
 import { mapState, mapMutations } from "vuex";
+import axios from "axios";
 
 export default {
+  async fetch({ store }) {
+    const res = await axios.get("https://todos-cuvsmolowg.now.sh/todos");
+    store.commit("init", res.data);
+  },
   computed: {
     ...mapState({
-      counter: state => state.counter
+      todos: state => state.todos
     })
   },
   methods: {
--- ssr/store/index.js
+++ ssr/store/index.js
@@ -1,12 +1,9 @@
 export const state = () => ({
-  counter: 0
+  todos: []
 });
 
 export const mutations = {
-  increment(state) {
-    state.counter++;
-  },
-  decrement(state) {
-    state.counter--;
+  init(state, todos) {
+    state.todos = todos;
   }
 };
